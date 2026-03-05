2fe85e71eaea12a25df973f95b42508529ba5ae4
vaanikpandit2825 <vaanikpandit@gmail.com>
2026-03-05 23:15:05 +0530

    UC10

---
+++ out/production/App/CaseInsensitive.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class UseCase10PalindromeCheckerApp {
+
+    public static boolean isPalindrome(String str) {
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+
+        boolean result = isPalindrome(normalized);
+
+        if (result) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+
+        sc.close();
+    }
+}
